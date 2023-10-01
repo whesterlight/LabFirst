1e7548bbf9849f2da992df013450eca6cee8aa9d
Mykyta Berezuiev <nikbere17@gmail.com>
2023-10-01 12:41:02 +0300

    Added button and popup

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Change(View view) {
+        Toast.makeText(this, "Some message", Toast.LENGTH_LONG).show();
     }
 }
