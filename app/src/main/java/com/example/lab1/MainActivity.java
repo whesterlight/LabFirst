7e12a676c8751d269c4142943a47cb2d301f8021
Mykyta Berezuiev <nikbere17@gmail.com>
2023-10-01 13:13:26 +0300

    Added proper button logic

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -4,22 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView Text;
+    private TextView TextWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.Text=findViewById(R.id.Text);
-    }
+        this.TextWindow = findViewById(R.id.TextWindow);
 
-    public void Change(View view) {
-        this.Text.setText("New text)");
+        Button Buttonchik = (Button) findViewById(R.id.Buttonchik);
+        Buttonchik.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                TextWindow.setText("New text)");
+            }
+        });
     }
 }
