de74c108a87ef3178c480eafa39ca170f2cfcbac
Mykyta Berezuiev <nikbere17@gmail.com>
2023-10-01 12:49:42 +0300

    Added button logic

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView Text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.Text=findViewById(R.id.Text);
     }
 
     public void Change(View view) {
-        Toast.makeText(this, "Some message", Toast.LENGTH_LONG).show();
+        this.Text.setText("New text)");
     }
 }
